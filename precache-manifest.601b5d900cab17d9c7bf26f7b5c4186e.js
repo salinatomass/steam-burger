30a11cdeaf13c9c577817b4a89fe84f005e0d7a3
Tomás Salina <salinatomass53@gmail.com>
2021-08-30 12:44:44 -0300

    Updates

---
+++ precache-manifest.601b5d900cab17d9c7bf26f7b5c4186e.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6f1b92af87eb3b0ad845d819fcef927",
+    "url": "/steam-burger/index.html"
+  },
+  {
+    "revision": "2e95ba3a91e0409320f5",
+    "url": "/steam-burger/static/css/main.4c0e6f43.chunk.css"
+  },
+  {
+    "revision": "983b291b03bdd9f8b711",
+    "url": "/steam-burger/static/js/2.538927bf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/steam-burger/static/js/2.538927bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e95ba3a91e0409320f5",
+    "url": "/steam-burger/static/js/main.f63783fd.chunk.js"
+  },
+  {
+    "revision": "289905d97c170ba0388c",
+    "url": "/steam-burger/static/js/runtime-main.c6224145.js"
+  },
+  {
+    "revision": "a973b7587003de54767c59d1f4e6a7dd",
+    "url": "/steam-burger/static/media/bg-large.a973b758.png"
+  },
+  {
+    "revision": "03430f3795bbb5989c57fe5692c756b1",
+    "url": "/steam-burger/static/media/bg-small.03430f37.png"
+  },
+  {
+    "revision": "c14da210ece1241c23521e65ebb6dad9",
+    "url": "/steam-burger/static/media/facebook.c14da210.svg"
+  },
+  {
+    "revision": "d86ffb96c0a0a2c9a8d405fec83fcace",
+    "url": "/steam-burger/static/media/fries.d86ffb96.svg"
+  },
+  {
+    "revision": "07318180b2834a0afc84d862d0a2c6a5",
+    "url": "/steam-burger/static/media/hamburger.07318180.svg"
+  },
+  {
+    "revision": "616a0a54916c7a6630cafebc0fb37855",
+    "url": "/steam-burger/static/media/ice-cream.616a0a54.svg"
+  },
+  {
+    "revision": "62593ffa4c7bb7261c7f40a15fb164c3",
+    "url": "/steam-burger/static/media/instagram.62593ffa.svg"
+  },
+  {
+    "revision": "ed0eaae7e8610ff57af27e39f14c818a",
+    "url": "/steam-burger/static/media/light-hamburger.ed0eaae7.svg"
+  },
+  {
+    "revision": "cea636c24fa8facb5189fec3dbb12866",
+    "url": "/steam-burger/static/media/nav.cea636c2.svg"
+  },
+  {
+    "revision": "a6c5fb57ac4f8072431fb22f874a17b5",
+    "url": "/steam-burger/static/media/soda.a6c5fb57.svg"
+  },
+  {
+    "revision": "b3de492a0068a424d5889011816104c8",
+    "url": "/steam-burger/static/media/tiktok.b3de492a.svg"
+  }
+]);
